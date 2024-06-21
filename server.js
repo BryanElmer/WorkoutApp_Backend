27f2324dd3ed814996d3335c84c9762ab2b9d275
Bryan Elmer <bryan.elmer.mulijono@gmail.com>
2024-06-22 01:17:39 +0800

    backend: convert to serverless

--- server.js
+++ server.js
@@ -22,14 +22,33 @@ app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
 // Connect to db
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    // Listen
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port 4000')
-    })
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+// mongoose.connect(process.env.MONGO_URI)
+//   .then(() => {
+//     // Listen
+//     app.listen(process.env.PORT, () => {
+//       console.log('connected to db & listening on port 4000')
+//     })
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   })
 
+let isConnected;
+
+async function connectToDatabase() {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log('connected to db');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connectToDatabase();
+
+module.exports = app;
