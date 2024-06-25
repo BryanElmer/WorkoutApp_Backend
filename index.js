b7369c3d2da373f991180e764549fa749f9ec3d9
Bryan Elmer <bryan.elmer.mulijono@gmail.com>
2024-06-25 14:03:25 +0800

    backend: deploy on vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 // Middleware
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -18,7 +18,6 @@ app.use((req, res, next) => {
 })
 
 // Routes
-app.get("/", (req, res) => res.send("Hello World!"));
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
 
@@ -34,22 +33,4 @@ mongoose.connect(process.env.MONGO_URI)
     console.log(error)
   })
 
-// let isConnected;
-
-// async function connectToDatabase() {
-//   if (isConnected) {
-//     return;
-//   }
-
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     isConnected = true;
-//     console.log('connected to db');
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-
-// connectToDatabase();
-
-module.exports = app;
+module.exports = app
